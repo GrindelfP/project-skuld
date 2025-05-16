4a13a68a45d08756f00cafe52a12ce992654a8c7
GrindelfP <grindelf.perlomutrovij@gmail.com>
2025-05-16 16:34:12 +0300

    [UPDATE] reformat code

--- cat/nn-python/genz-plotter.py
+++ cat/nn-python/genz-plotter.py
@@ -1,5 +1,5 @@
 ########################################
-   ####### FUNCTIONS PLOTTER #######
+####### FUNCTIONS PLOTTER #######
 ########################################
 
 from genz.genz_functions import *
@@ -13,36 +13,39 @@ cc: list[float] = [c, c]
 num_points: int = 500
 unit_cube_range: tuple[int, int] = (0, 1)
 
+
 ############ PROGRAM ############
 def plot_genz():
-        ### 1D CASE
-        plot_line(
-            func=disco_1d,
-            x_range=unit_cube_range,
-            num_points=500,
-            title=f'DISCONTINUOUS: u = {u}, c = {c}.',
-            x_label='X',
-            y_label='Y',
-            u=u,
-            c=c
-        ).show()
+    ### 1D CASE
+    plot_line(
+        func=gauss_1d,
+        x_range=unit_cube_range,
+        num_points=500,
+        title=f'DISCONTINUOUS: u = {u}, c = {c}.',
+        x_label='X',
+        y_label='Y',
+        u=u,
+        c=c
+    ).show()
 
-        ### 2D CASE
-        plot_surface(
-            func=disco_2d,
-            x_range=unit_cube_range,
-            y_range=unit_cube_range,
-            num_points=num_points,
-            title=f'DISCONTINUOUS: u = {uu}, c = {cc}.',
-            x_label='X',
-            y_label='Y',
-            z_label='Z',
-            u=uu,
-            c=cc
-        ).show()
+    ### 2D CASE
+    plot_surface(
+        func=gauss_2d,
+        x_range=unit_cube_range,
+        y_range=unit_cube_range,
+        num_points=num_points,
+        title=f'DISCONTINUOUS: u = {uu}, c = {cc}.',
+        x_label='X',
+        y_label='Y',
+        z_label='Z',
+        u=uu,
+        c=cc
+    ).show()
+
 
 def main():
     plot_genz()
+
 
 if __name__ == '__main__':
     main()
